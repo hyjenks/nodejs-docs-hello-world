2a66c84b9e5df947d7bbd5243d98c22c17892672
Bryan Jenks <bryan@droplit.io>
2020-05-03 15:30:09 -0400

    poc yarn workspace deps

--- packages/websvr/index.js
+++ packages/websvr/index.js
@@ -1,8 +1,9 @@
 const http = require('http');
+const version = require('@company/versionlib');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! 01");
+    response.end("Hello World!" + version.number);
 });
 
 const port = process.env.PORT || 1337;
